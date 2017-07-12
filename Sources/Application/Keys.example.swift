17344dbcab89cb54780f97bbf1c8d62afa215393
Daniel Romatsky <murkwie@gmail.com>
2017-07-12 22:17:01 +0700

    Ignore & remove Keys.swift, add Keys.example.swift

---
+++ Sources/Application/Keys.example.swift
@@ -0,0 +1,15 @@
+//
+//  Keys.swift
+//  MementoFM
+//
+//  Created by Daniel on 14/10/16.
+//  Copyright © 2016 icecoffin. All rights reserved.
+//
+
+import Foundation
+
+struct Keys {
+  struct LastFM {
+    static let apiKey = "YOUR_API_KEY"
+  }
+}
