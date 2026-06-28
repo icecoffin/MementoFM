8e1d89bb0116d80606667f743988c4cf502983fe
Daniil Romatskiy <murkwie@gmail.com>
2026-06-28 14:21:05 +0200

    Adopt UIScene lifecycle

--- Sources/Application/AppDelegate.swift
+++ Sources/Application/AppDelegate.swift
@@ -10,32 +10,16 @@ import UIKit
 
 let log = Logger.self
 
-@UIApplicationMain
+@main
 final class AppDelegate: UIResponder, UIApplicationDelegate {
-    var window: UIWindow?
-    private var appCoordinator: AppCoordinator?
-
     func application(
         _ application: UIApplication,
-        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
-    ) -> Bool {
-        if NSClassFromString("XCTestCase") != nil {
-            return true
-        }
-
-        log.debug(NSHomeDirectory())
-
-        let window = UIWindow(frame: UIScreen.main.bounds)
-        window.backgroundColor = .systemBackground
-        AppearanceConfigurator.configureAppearance()
-
-        let appCoordinator = AppCoordinator(window: window)
-        appCoordinator.start()
-        window.makeKeyAndVisible()
-
-        self.window = window
-        self.appCoordinator = appCoordinator
-
-        return true
+        configurationForConnecting connectingSceneSession: UISceneSession,
+        options: UIScene.ConnectionOptions
+    ) -> UISceneConfiguration {
+        return UISceneConfiguration(
+            name: "Default Configuration",
+            sessionRole: connectingSceneSession.role
+        )
     }
 }
---
+++ Sources/Application/SceneDelegate.swift
@@ -0,0 +1,34 @@
+//
+//  SceneDelegate.swift
+//  MementoFM
+//
+//  Created by Dani on 28.06.26.
+//
+
+import UIKit
+
+final class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+    var window: UIWindow?
+    private var appCoordinator: AppCoordinator?
+
+    func scene(
+        _ scene: UIScene,
+        willConnectTo session: UISceneSession,
+        options connectionOptions: UIScene.ConnectionOptions
+    ) {
+        guard let windowScene = scene as? UIWindowScene else { return }
+
+        let window = UIWindow(windowScene: windowScene)
+        window.backgroundColor = .systemBackground
+        AppearanceConfigurator.configureAppearance()
+
+        let appCoordinator = AppCoordinator(window: window)
+        appCoordinator.start()
+        window.makeKeyAndVisible()
+
+        log.debug(NSHomeDirectory())
+
+        self.window = window
+        self.appCoordinator = appCoordinator
+    }
+}
